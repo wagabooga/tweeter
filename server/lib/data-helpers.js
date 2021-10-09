03289418828abc1721edd7291024c7d467f27bc1
wagabooga <mattanthu@hotmail.com>
2021-10-09 05:50:06 +0000

    added text-input container and styled

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -11,6 +11,7 @@ module.exports = function makeDataHelpers(db) {
     saveTweet: function(newTweet, callback) {
       simulateDelay(() => {
         db.tweets.push(newTweet);
+        console.log(db.tweets)
         callback(null, true);
       });
     },
