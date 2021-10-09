96ab71271e5a92d2d666afe4bf99bfe77ad29451
wagabooga <mattanthu@hotmail.com>
2021-10-09 22:39:38 +0000

    composer-char-counter.js counter bug fix, as well as removed line-top on tweet text box

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
-  $("#tweet-text").on("input",function(){
-    let charactersRemaining = 140 - $(this).val().length 
+  $("#tweet-text").on("input", function() {
+    let charactersRemaining = 140 - $(this).val().length
     $(".counter").text(charactersRemaining)
-    if (charactersRemaining < 0){
-      $(".counter").css("color", "red")
-      $(".counter").css("font-weight", "800")
-      $(".counter").css("text-decoration", "underline")
+    if (charactersRemaining < 0) {
+      $(".counter").addClass("error")
+    } else {
+      $(".counter").removeClass("error")
     }
+    //   $(".counter").css("color", "red")
+    //   $(".counter").css("font-weight", "800")
+    //   $(".counter").css("text-decoration", "underline")
+    // }
+    // if (charactersRemaining >= 0) {
+    //   $(".counter").css("color", "green")
+    //   $(".counter").css("font-weight", "800")
+    //   $(".counter").css("text-decoration", "underline")
+    // }
     console.log(charactersRemaining)
   });
 });
